a7fe0f64828c6a46670d720b6daa228f26197d16
hamdan <hamdandirgantara@gmail.com>
2025-10-03 23:35:56 +0700

    feat: assign prettier as typescript, typescriptreact, javascriptreact formatter

--- init.lua
+++ init.lua
@@ -38,6 +38,9 @@ require("conform").setup({
 		--rust = { "rustfmt", lsp_format = "fallback" },
 		-- Conform will run the first available formatter
 		javascript = { "prettierd", "prettier", stop_after_first = true },
+		javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+		typescript = { "prettierd", "prettier", stop_after_first = true },
+		typescriptreact = { "prettierd", "prettier", stop_after_first = true },
 		html = { 'prettier' },
 		php = { 'pint' },
 		blade = { 'blade-formatter' }
